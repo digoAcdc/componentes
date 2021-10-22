eadfe6c0854c06162d9a89ea81010e0ebcc13aec
rodrigosilveirabarbosa <barbosa.silveira@gmail.com>
2021-10-22 10:31:52 -0300

    teste

--- lib/butons/circular_button.dart
+++ lib/butons/circular_button.dart
@@ -5,19 +5,6 @@ class CircularButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-        height: 50,
-        decoration: new BoxDecoration(
-            color: Colors.green,
-            borderRadius: new BorderRadius.only(
-              topLeft: const Radius.circular(40.0),
-              topRight: const Radius.circular(40.0),
-            )
-        ),
-        child: new Center(
-          child: new Text("Hi modal sheet"),
-        )
-    );
+    return OutlinedButton(child: Text("botao"), onPressed: (){},);
   }
 }
